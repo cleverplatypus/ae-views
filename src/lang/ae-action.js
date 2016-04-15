3a0d85c3d2e42909a8745d45e5bd1c7e8bbcd431
Nicola Dal Pont <ndalpont@gmail.com>
2016-04-15 11:46:48 +0930

    Started works to add parameters to ae-action

--- src/lang/ae-action.js
+++ src/lang/ae-action.js
@@ -4,6 +4,32 @@ import $ from 'jquery';
 import Element from './ae-element';
 import _ from 'lodash';
 
+
+var typifyParams = function typifyParams(inParams) {
+    var out = {};
+    _.each(inParams, function(inParamValue, inParamKey) {
+        if (!inParamValue) {
+            out[inParamKey] = null;
+        } else if(_.isString(inParamValue) && /^`.*`$/.test(inParamValue)) {
+            out[inParamKey] = inParamValue.replace(/^`/,'').replace(/`$/,'');
+        } else if (!isNaN(inParamValue)) {
+            out[inParamKey] = Number(inParamValue);
+        } else if (/^(true|false)$/.test(inParamValue)) {
+            out[inParamKey] = (inParamValue === 'true');
+        } else {
+            console.warn('using deprecated signal string param format');
+            out[inParamKey] = inParamValue; //is a string
+        }
+
+    });
+    return out;
+};
+
+var assembleParams = function(inActionNode) {
+    return typifyParams(params);
+};
+
+
 export default function action(inPage) {
     const _page = inPage;
 
@@ -14,7 +40,10 @@ export default function action(inPage) {
         const actionName = $(this).attr('name');
         const component = _page.resolveNodeComponent(target);
         $(target).click(() => {
-            component.bus.triggerAction(actionName);
+            component.bus.triggerAction( 
+                actionName, 
+                target,
+                assembleParams());
         });
 
 
